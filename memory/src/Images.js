91e5b4d79913ad03eeaa439b7d0f2c64a5b81049
MikolajSroda <95876594+MikolajSroda@users.noreply.github.com>
2022-01-18 10:09:41 +0100

    Add files via upload

---
+++ memory/src/Images.js
@@ -0,0 +1,40 @@
+import Image01 from './photos/01.jpg';
+import Image02 from './photos/02.jpg';
+import Image03 from './photos/03.jpg';
+import Image04 from './photos/04.jpg';
+import Image05 from './photos/05.jpg';
+import Image06 from './photos/06.jpg';
+import Image07 from './photos/07.jpg';
+import Image08 from './photos/08.jpg';
+import Image09 from './photos/09.jpg';
+import Image10 from './photos/10.jpg';
+import Image11 from './photos/11.jpg';
+import Image12 from './photos/12.jpg';
+import Image13 from './photos/13.jpg';
+import Image14 from './photos/14.jpg';
+import Image15 from './photos/15.jpg';
+import Image16 from './photos/16.jpg';
+import Image17 from './photos/17.jpg';
+import Image18 from './photos/18.jpg';
+
+const Images = [
+     Image01,
+      Image02,
+       Image03,
+        Image04,
+         Image05,
+          Image06,
+           Image07,
+            Image08,
+             Image09,
+              Image10,
+               Image11,
+                Image12,
+                 Image13,
+                  Image14,
+                   Image15,
+                    Image16,
+                     Image17,
+                      Image18 ];
+
+                     export default Images;
